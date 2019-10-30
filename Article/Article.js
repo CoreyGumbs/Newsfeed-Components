750f20e7b65995f955cecefcc2feaadf5a2c034c
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-10-30 17:52:15 -0400

    [Add] Created Article Elements

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,15 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+const article = (title, date, paragraph1, paragraph2, paragraph3) => {
+  const articleCard = document.createElement('div');
+  const articleTitle = document.createElement('h2');
+  const articleParagraph1 = document.createElement('p');
+  const articleParagraph2 = document.createElement('p');
+  const articleParagraph3 = document.createElement('p');
+  const articleExpandBtn = document.createElement('span');
+
+  return articleCard;
+
+}
